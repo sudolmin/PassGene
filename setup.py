09524e8bcca4780f22d40dd5410002eccd9d5ff0
sudolmin <61926180+sudolmin@users.noreply.github.com>
2020-03-08 09:11:01 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 import os
 from cx_Freeze import setup, Executable
 
-os.environ['TCL_LIBRARY'] = 'C:/Users/Durgawati Sahu/AppData/Local/Programs/Python/Python38-32/tcl/tcl8.6'
-os.environ['TK_LIBRARY'] = 'C:/Users/Durgawati Sahu/AppData/Local/Programs/Python/Python38-32/tcl/tk8.6'
+os.environ['TCL_LIBRARY'] = 'C:/Users/Python/Python38-32/tcl/tcl8.6'
+os.environ['TK_LIBRARY'] = 'C:/Users/Python/Python38-32/tcl/tk8.6'
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
 buildOptions = dict(
     packages = [],
     excludes = [],
-    include_files=['C:/Users/Durgawati Sahu/AppData/Local/Programs/Python/Python38-32/DLLs/tcl86t.dll',
-                   'C:/Users/Durgawati Sahu/AppData/Local/Programs/Python/Python38-32/DLLs/tk86t.dll']
+    include_files=['C:/Users/Programs/Python/Python38-32/DLLs/tcl86t.dll',
+                   'C:/Users/Programs/Python/Python38-32/DLLs/tk86t.dll']
 )
 
 executables = [
